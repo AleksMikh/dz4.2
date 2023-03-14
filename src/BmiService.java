2939806fb23ac0d1facbbb0881ef2202d0cdd75d
Mikhail Alekseev <rai1gun@mail.ru>
2023-03-14 17:15:50 +0300

    main and bmiservice changed

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
     public int calculate(int weight, double height) {
-        double myBmi = weight/(height*height);
+        double myBmi = weight / (height * height);
         return (int) myBmi;
     }
 }
